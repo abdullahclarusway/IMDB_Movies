ff1b410d50ae7abb8e8feb61e0bd580039cc5962
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2020-11-14 17:42:31 +0100

    Initialize Project

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import './App.css';
+import {SearchBox} from "./components/SearchBox";
+import {CardList} from "./components/CardList";
+
+const apiKey = "c8015b69e437a68b1b37cfb3bd3f2b19"; //temporary
+const baseUrl = "https://api.themoviedb.org/3/search/movie";
+const baseImageUrl = "https://image.tmdb.org/t/p/w500";
+
+
+function App() {
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(()=>{
+      axios.get(
+          baseUrl, {params: {
+          api_key: apiKey,
+          page: 1,
+          query: "Star Wars"  //TODO: from input
+          }}
+      )
+      .then((res)=> setMovieList(res?.data?.results))
+      .catch()
+      .finally()
+  },[])
+
+  return (
+    <div className="App">
+      <SearchBox/>
+      <CardList movieList={movieList} baseImageUrl={baseImageUrl} />
+    </div>
+  );
+}
+
+export default App;

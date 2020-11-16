5823337f4bea31cff744b83bccb1e19c48622e27
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2020-11-16 22:37:09 +0100

    Add React Router Dom and Link

---
+++ src/Movie.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect, createContext } from "react";
+import axios from "axios";
+
+import './App.css';
+import {SearchBox} from "./components/SearchBox";
+import {CardList} from "./components/CardList";
+
+const apiKey = "c8015b69e437a68b1b37cfb3bd3f2b19"; //temporary
+const baseUrl = "https://api.themoviedb.org/3/search/movie";
+const baseImageUrl = "https://image.tmdb.org/t/p/w500";
+
+export const MovieContext  = createContext();
+
+function Movie() {
+
+  const [movieList, setMovieList] = useState([]);
+  const [searchKeyword, setSearchKeyword] = useState("Matrix")
+
+  useEffect(()=>{
+      axios.get(
+          baseUrl, {params: {
+          api_key: apiKey,
+          page: 1,
+          query: searchKeyword
+          }}
+      )
+      .then((res)=> setMovieList(res?.data?.results))
+      .catch()
+      .finally()
+  },[searchKeyword])
+
+  return (
+    <div className="App">
+      <MovieContext.Provider value={{ movieList, baseImageUrl, setSearchKeyword }}>
+          <SearchBox />
+          <CardList />
+      </MovieContext.Provider>
+    </div>
+  );
+}
+
+export default Movie;
---
+++ src/MovieDetails.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { StyledMovieImage} from "./components/Card/Card.style"
+
+const movieDetailBaseUrl = "https://api.themoviedb.org/3/movie/"
+const apiKey = "c8015b69e437a68b1b37cfb3bd3f2b19"; //temporary
+const baseImageUrl = "https://image.tmdb.org/t/p/w500";
+
+export default function MovieDetails() {
+    const [movieDetails, setMovieDetails] = useState()
+    const { id } = useParams();
+
+    useEffect(()=>{
+        axios.get(
+            movieDetailBaseUrl+id, {params: {
+            api_key: apiKey,
+            }}
+        )
+        .then(res=>setMovieDetails(res?.data))
+        .catch()
+        .finally()
+    },[id])
+
+    return (
+        <div>
+            <h1>{movieDetails?.original_title}</h1>
+            <StyledMovieImage src={baseImageUrl+movieDetails?.poster_path} alt={"Movie Poster"}/>
+            <p>{movieDetails?.overview}</p> 
+        </div>
+    )
+}

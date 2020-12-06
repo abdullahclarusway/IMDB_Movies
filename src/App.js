19eacc542c424c8a4b60eb972a84decd030da993
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2020-12-06 23:39:22 +0100

    Add Link

--- src/App.js
+++ src/App.js
@@ -1,39 +1,17 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import Movie from "./Movie"
+import MovieDetails from "./MovieDetails"
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Navbar from "./components/Navbar"
 
-import './App.css';
-import {SearchBox} from "./components/SearchBox";
-import {CardList} from "./components/CardList";
-
-const apiKey = "c8015b69e437a68b1b37cfb3bd3f2b19"; //temporary
-const baseUrl = "https://api.themoviedb.org/3/search/movie";
-const baseImageUrl = "https://image.tmdb.org/t/p/w500";
-
-
-function App() {
-
-  const [movieList, setMovieList] = useState([]);
-  const [searchKeyword, setSearchKeyword] = useState("Matrix")
-
-  useEffect(()=>{
-      axios.get(
-          baseUrl, {params: {
-          api_key: apiKey,
-          page: 1,
-          query: searchKeyword
-          }}
-      )
-      .then((res)=> setMovieList(res?.data?.results))
-      .catch()
-      .finally()
-  },[searchKeyword])
-
-  return (
-    <div className="App">
-      <SearchBox setSearchKeyword={setSearchKeyword}/>
-      <CardList movieList={movieList} baseImageUrl={baseImageUrl} />
-    </div>
-  );
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Navbar/>
+            <Switch>
+                <Route path="/detail/:id" component={MovieDetails} exact/>
+                <Route path="/" component={Movie}/>
+            </Switch>
+        </BrowserRouter>
+    )
 }
-
 export default App;
